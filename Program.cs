e524e37df2c8dfc598e3b6bebdaa772dc4664000
Sergey Kalinin <1streee.prod@gmail.com>
2026-02-04 00:29:35 +0700

    Initial bot skeleton

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Polling;
+
+var token = "8458353334:AAGLS1kGunBNVW4JYuOP103mVB1tqCP3j_0";
+
+var bot = new TelegramBotClient(token);
+
+using var cts = new CancellationTokenSource();
+
+bot.StartReceiving(
+    HandleUpdateAsync,
+    HandleErrorAsync,
+    cancellationToken: cts.Token
+);
+
+var me = await bot.GetMe();
+Console.WriteLine($"Bot @{me.Username} started");
+
+Console.ReadLine();
+cts.Cancel();
+
+async Task HandleUpdateAsync(
+    ITelegramBotClient bot,
+    Update update,
+    CancellationToken ct
+)
+{
+    if (update.Message is { Text: { } text})
+    {
+        await bot.SendMessage(
+            update.Message.Chat.Id,
+            $"Ты написал: {text}"
+        );
+    };
+}
+
+Task HandleErrorAsync(
+    ITelegramBotClient bot,
+    Exception exception,
+    CancellationToken ct
+)
+{
+    Console.WriteLine(exception.Message);
+    return Task.CompletedTask;
+};
